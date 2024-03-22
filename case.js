314cdbb7a2cbf6d20a4b6a7681e32257c56cdb72
ThelysTuratto <164553748+ThelysTuratto@users.noreply.github.com>
2024-03-22 16:35:31 -0300

    Add files via upload

---
+++ case.js
@@ -0,0 +1,45 @@
+
+//Se o cliente selecionar uma opção válida:
+
+var bebida = 1;
+
+
+ switch(bebida){
+    case 1:
+             console.log("café:5,99 reais");
+      break;
+   
+      case 2:
+             console.log("leite, 6,99 reais");
+      break;
+
+    case 3:
+             console.log("chá, 7,99 reais");
+      break;
+
+    default:
+      console.log("A opção selecionada é inválida. Escolha entre café, leite ou chá.");
+     }
+  
+  // Se o cliente selecionar uma opção inválida
+
+  var bebida = 8;
+
+
+ switch(bebida){
+    case 1:
+             console.log("café:5,99reais");
+      break;
+   
+      case 2:
+             console.log("leite, 6,99 reais");
+      break;
+
+    case 3:
+             console.log("chá, 7,99 reais");
+      break;
+
+    default:
+      console.log("A opção selecionada é inválida. Escolha entre café, leite ou chá.");
+     }
+
